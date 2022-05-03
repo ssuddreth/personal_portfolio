6a7347a3e7c6e2b4d2772d641c2ba76948ae6ecd
Steven Suddreth <stevensuddreth@gmail.com>
2022-05-03 18:18:36 -0400

    complete Records section

--- assets/app.js
+++ assets/app.js
@@ -1,6 +1,8 @@
 const filter_btns = document.querySelectorAll(".filter-btn");
 const skills_wrap = document.querySelector(".skills");
 const skills_bars = document.querySelectorAll(".skill-progress");
+const records_wrap = document.querySelector(".records");
+const records_numbers = document.querySelectorAll(".number");
 
 filter_btns.forEach(btn => btn.addEventListener("click", () => {
     filter_btns.forEach((button) => button.classList.remove('active'));
@@ -21,6 +23,7 @@ $('.grid').isotope({
 
   window.addEventListener("scroll", () => {
       skillsEffect();
+      countUp();
   })
 
   function checkScroll(el) {
@@ -32,4 +35,26 @@ $('.grid').isotope({
   function skillsEffect() {
       if(!checkScroll(skills_wrap)) return;
       skills_bars.forEach((skill) => (skill.style.width = skill.dataset.progress));
+  }
+
+  function countUp() {
+    if(!checkScroll(records_wrap)) return;
+    records_numbers.forEach((numb) => {
+        const updateCount = () => {
+            let currentNum = +numb.innerText;
+            let maxNum = +numb.dataset.num;
+            let speed = 100;
+            const increment = Math.ceil(maxNum / speed);
+
+            if (currentNum < maxNum) {
+                numb.innerText = currentNum + increment;
+                setTimeout(updateCount, 1);
+            }
+            else {
+                numb.innerText = maxNum;
+            }
+        }
+
+        setTimeout(updateCount, 400);
+    });
   }
