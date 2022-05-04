46cd7ca82c8afa62159f511beb47016b8f9dbbd4
Steven Suddreth <stevensuddreth@gmail.com>
2022-05-04 19:44:11 -0400

    complete Testimonial, Contact, and Hire Me sections

--- assets/app.js
+++ assets/app.js
@@ -58,3 +58,16 @@ $('.grid').isotope({
         setTimeout(updateCount, 400);
     });
   }
+
+  var mySwiper = new Swiper(".swiper-container", {
+      speed: 1100, 
+      slidesPerView: 1,
+      loop: true,
+      autoplay: {
+          delay: 5000,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+  });
